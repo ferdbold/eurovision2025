9ed6344476e4d900a1ce752b0484389932604b39
Frédéric Bolduc <me@ferdbold.com>
2025-03-02 21:32:17 -0500

    Setup basic pages

---
+++ app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminView() {
+	return (
+		<div className="w-full h-screen flex flex-col items-center justify-center">
+			<h1>Vue admin</h1>
+		</div>
+	);
+}
---
+++ app/tv/page.tsx
@@ -0,0 +1,7 @@
+export default function TelevisionView() {
+	return (
+		<div className="w-full h-screen flex flex-col items-center justify-center">
+			<h1>Vue télé</h1>
+		</div>
+	);
+}
