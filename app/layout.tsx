4f1ef29f62224fac1c004ec5f957abf34b02c380
Frédéric Bolduc <me@ferdbold.com>
2025-04-26 14:17:20 -0400

    Fix dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "En attendant l'Eurovision"
@@ -24,7 +13,7 @@ export default function RootLayout({children}: Readonly<{
 {
   return (
     <html lang="fr">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className="font-regular antialiased">
         {children}
       </body>
     </html>
