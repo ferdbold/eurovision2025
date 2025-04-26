8ae34073ddb61511eb81602f3eca28fb37656be6
Frédéric Bolduc <me@ferdbold.com>
2025-04-26 15:00:55 -0400

    add missing suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 'use client';
 
+import {useSearchParams} from "next/navigation";
+import {Suspense} from "react";
 import {Button} from "@/app/ui/button";
 import {signup} from "@/app/lib/auth";
-import {useSearchParams} from "next/navigation";
 
 export default function Home() {
   const searchParams = useSearchParams();
 
   return (
-    <div className="w-full p-4 h-screen flex flex-col items-center justify-center gap-4">
-      {searchParams.has('error') && <div className="w-full px-4 py-2 rounded bg-red-900 border border-red-500">{searchParams.get('error')}</div>}
-      <Login />
-    </div>
+    <Suspense>
+      <div className="w-full p-4 h-screen flex flex-col items-center justify-center gap-4">
+        {searchParams.has('error') && <div className="w-full px-4 py-2 rounded bg-red-900 border border-red-500">{searchParams.get('error')}</div>}
+        <Login />
+      </div>
+    </Suspense>
   );
 }
 
