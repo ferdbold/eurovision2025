cfb31e0eea229d78239b5f848c5f4f0521902100
Frédéric Bolduc <me@ferdbold.com>
2025-04-26 14:03:55 -0400

    Improve look of drag entries

--- app/vote/dragEntry.tsx
+++ app/vote/dragEntry.tsx
@@ -23,15 +23,22 @@ export default function DragEntry(props: IDragEntryProps) {
 		<div ref={setNodeRef}
 				 className="mx-2 gap-2 rounded-xl bg-gradient-to-r from-pink-400 to-blue-800"
 				 style={style} {...listeners} {...attributes}>
-			<div className="flex flex-row gap-2 items-center m-1 pr-4 bg-gradient-to-r from-pink-600 to-blue-950 rounded-lg">
-				<div className="text-6xl text-center w-16 font-fancy font-bold select-none">{props.position}</div>
-				<div className="grow leading-5 select-none">
-					<div className="text-3xl leading-7 font-fancy font-bold">{props.performance.performer}</div>
-					<div><strong>{props.performance.song}</strong>, <em>{props.performance.artist}</em></div>
-					<div><strong>{props.performance.country}</strong>, {props.performance.year}</div>
+			<div className="flex flex-row items-center gap-1">
+				<div className="flex flex-row grow gap-0 items-center relative m-1 pr-2 py-2 bg-gradient-to-r from-pink-600 to-blue-950 rounded-lg rounded-tr-none rounded-br-none">
+					<div className="text-5xl text-center w-16 font-fancy font-bold select-none">{props.position}</div>
+					<div className="grow leading-5 select-none z-10">
+						<div className="text-3xl leading-7 font-fancy font-bold">{props.performance.performer}</div>
+						<div><strong>{props.performance.song}</strong></div>
+						<div><em>{props.performance.artist}</em></div>
+						<div><strong>{props.performance.country}</strong>, {props.performance.year}</div>
+					</div>
+					<div className="absolute flex w-full h-full justify-end gap-2 opacity-70">
+						{props.performance.image_urls.map((url, i) =>
+							<img key={`performer-img-${i}`} src={url} alt={props.performance.performer} className="max-w-32" />
+						)}
+					</div>
 				</div>
-				{props.performance.image_urls.map(url => <img src={url} alt={props.performance.performer} className="max-w-16" />)}
-				<Bars3Icon className="size-6"/>
+				<Bars3Icon className="size-6 mr-2"/>
 			</div>
 		</div>
 	);
--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -121,7 +121,7 @@ export default function TelevisionView() {
 				<h1 className="font-bold text-black">{userId}</h1>
 			</div>
 
-			<div className="flex flex-col gap-2 mt-2 mr-8">
+			<div className="flex flex-col gap-2 mt-2">
 				<DndContext onDragEnd={handleDragEnd} sensors={sensors} modifiers={[restrictToVerticalAxis]}>
 					<SortableContext items={performers} strategy={verticalListSortingStrategy}>
 						{performers.map((p, i) => <DragEntry key={p.id} position={i+1} performance={p}></DragEntry> )}
